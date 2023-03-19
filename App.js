fdb09fc8ae9c53581286d726e75e1d10018157a5
Kelsey Brown <119462221+KelseyBrown90s@users.noreply.github.com>
2023-03-18 20:40:24 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+function App() {
+  return (
+    <><Navbar></Navbar><LoginForm></LoginForm></>    
+  );
+}
+
+
+
+
+function Navbar(){
+ 
+      <ul style="list-style-type: none;margin: 0;padding: 0;">
+<li><a href="homehalloween.html">Home</a></li>
+<li><a href="aboutHalloween.html">News</a></li>
+<li><a href="contacthalloween.html">Contact</a></li>
+</ul>
+}
+  //this is my section for navbar and it features html sites I have created.
+
+<br></br>
+function LoginForm (){
+  
+      
+         <><h3>"Login"</h3><input type="text" placeholder="username"></input><input type="text" placeholder="password"></input></>   
+}
+//This section is my login form attempt and  I did text inputs , and I believe some examples had a password input type, but I decided to go with text.
+export default App;
